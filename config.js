8a8f623cf98b6bad8785a894e6c3b05b964a4651
Craigee2k25 <151790551+Craigee2k25@users.noreply.github.com>
2023-11-23 00:04:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['6281255369012', 'Craigee_zw', true], 
   [''] 
 ] //Number of owners
 
